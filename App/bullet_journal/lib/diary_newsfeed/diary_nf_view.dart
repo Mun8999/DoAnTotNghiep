e4a7dfbbc27937e0f073f2025eabe95e864db423
Mun8999 <62742708+Mun8999@users.noreply.github.com>
2021-05-01 23:42:38 +0700

    sua giao dien

--- App/bullet_journal/lib/diary_newsfeed/diary_nf_view.dart
+++ App/bullet_journal/lib/diary_newsfeed/diary_nf_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DiaryNewFeedsView extends StatefulWidget {
   @override
@@ -157,150 +158,170 @@ class _DiaryNewFeedsViewState extends State<DiaryNewFeedsView> {
           )
         ];
       },
-      body: SingleChildScrollView(
-        physics: NeverScrollableScrollPhysics(),
-        child: Container(
-            height: size.height,
-            color: Colors.white,
-            child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: images.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 250,
-                    child: Stack(children: [
-                      Container(
-                        height: 250,
+      body: Container(
+          height: size.height,
+          color: Colors.white,
+          child: ListView.builder(
+            /// de o day de khong scroll theo cai nay
+            physics: NeverScrollableScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            itemCount: images.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 250,
+                  child: Stack(children: [
+                    Container(
+                      height: 250,
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          // color: 100 * (index % 9) == 0
+                          //     ? Colors.pink[50]
+                          //     : Colors.pink[100 * (index % 9)],
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                blurRadius: 1,
+                                offset: Offset(2, 2))
+                          ]),
+                      // child:
+                    ),
+                    Positioned(
+                      top: 10,
+                      left: index % 2 == 0 ? null : 10,
+                      right: index % 2 != 0 ? null : 10,
+                      child: Stack(
+                        children: [
+                          Container(
+                            height: 200,
+                            width: 150,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: 100 * (index % 9) == 0
+                                    ? Colors.yellow[50]
+                                    : Colors.yellow[100 * (index % 9)]),
+                          ),
+                          Column(
+                            children: [
+                              Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Text('Đom Đóm - Jack',
+                                      style: GoogleFonts.dancingScript(
+                                          fontSize: 16))),
+                              Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Text(
+                                      '" Người giờ còn đây không?\nThuyền này liệu còn sang sông?\nBuổi chiều dài mênh mông\nLòng người giờ hòa hay đông?\nHồng mắt em cả bầu trời đỏ hoen\nTa như đứa trẻ ngây thơ\nQuên đi tháng ngày ngu ngơ... "',
+                                      style: GoogleFonts.dancingScript(
+                                          fontSize: 11)))
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      top: 10,
+                      left: index % 2 == 0 ? 10 : null,
+                      right: index % 2 != 0 ? 10 : null,
+                      child: Container(
+                        height: 200,
+                        width: 200,
                         decoration: BoxDecoration(
-                            color: Colors.black,
-                            // color: 100 * (index % 9) == 0
-                            //     ? Colors.pink[50]
-                            //     : Colors.pink[100 * (index % 9)],
-                            borderRadius: BorderRadius.circular(8),
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  blurRadius: 1,
-                                  offset: Offset(2, 2))
-                            ]),
-                        // child:
-                      ),
-                      Positioned(
-                        top: 10,
-                        left: index % 2 == 0 ? null : 10,
-                        right: index % 2 != 0 ? null : 10,
-                        child: Container(
-                          height: 200,
-                          width: 150,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.yellow[300]),
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: AssetImage(images[index]),
+                              fit: BoxFit.cover),
                         ),
                       ),
-                      Positioned(
-                        top: 10,
-                        left: index % 2 == 0 ? 10 : null,
-                        right: index % 2 != 0 ? 10 : null,
-                        child: Container(
-                          height: 200,
-                          width: 200,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            image: DecorationImage(
-                                image: AssetImage(images[index]),
-                                fit: BoxFit.cover),
+                    ),
+                    Positioned(
+                      bottom: 5,
+                      left: index % 2 == 0 ? 10 : null,
+                      right: index % 2 != 0 ? 10 : null,
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Text(
+                            '09-09-2021',
+                            style: TextStyle(
+                                color: Colors.white.withOpacity(0.7),
+                                fontSize: 16),
                           ),
                         ),
                       ),
-                      Positioned(
-                        bottom: 5,
-                        left: index % 2 == 0 ? 10 : null,
-                        right: index % 2 != 0 ? 10 : null,
-                        child: Container(
-                          alignment: Alignment.centerLeft,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8),
-                            child: Text(
-                              '09-09-2021',
-                              style: TextStyle(
-                                  color: Colors.white.withOpacity(0.7),
-                                  fontSize: 16),
-                            ),
-                          ),
-                        ),
-                      )
-                    ]),
-                  ),
-                );
-              },
-            )
-            //   StaggeredGridView.countBuilder(
-            //     // controller: _staggredGridController,
-            //     physics:
-            //         NeverScrollableScrollPhysics(), // không scroll theo widget này
-            //     // shrinkWrap: true,
-            //     scrollDirection: Axis.vertical,
-            //     crossAxisCount: 4,
-            //     itemCount: images.length,
-            //     itemBuilder: (BuildContext context, int index) {
-            //       return Container(
-            //         child: Stack(children: [
-            //           Container(
-            //             padding:
-            //                 EdgeInsets.only(top: 5, left: 5, right: 5, bottom: 50),
-            //             decoration: BoxDecoration(
-            //                 color: Colors.white,
-            //                 // color: 100 * (index % 9) == 0
-            //                 //     ? Colors.pink[50]
-            //                 //     : Colors.pink[100 * (index % 9)],
-            //                 borderRadius: BorderRadius.circular(8),
-            //                 boxShadow: [
-            //                   BoxShadow(
-            //                       color: Colors.grey.withOpacity(0.5),
-            //                       blurRadius: 1,
-            //                       offset: Offset(2, 2))
-            //                 ]),
-            //             child: SizedBox(
-            //               child: Container(
-            //                 decoration: BoxDecoration(
-            //                   borderRadius: BorderRadius.circular(8),
-            //                   image: DecorationImage(
-            //                       image: AssetImage(images[index]),
-            //                       fit: BoxFit.cover),
-            //                 ),
-            //               ),
-            //             ),
-            //           ),
-            //           Positioned(
-            //             bottom: 8,
-            //             child: Container(
-            //               alignment: Alignment.centerLeft,
-            //               child: Padding(
-            //                 padding: const EdgeInsets.all(8),
-            //                 child: Text(
-            //                   'Hellllo',
-            //                   style: TextStyle(color: Colors.black, fontSize: 16),
-            //                 ),
-            //               ),
-            //             ),
-            //           )
-            //         ]),
-            //       );
-            //     },
-            //     // ignore: missing_return
-            //     staggeredTileBuilder: (index) {
-            //       return new StaggeredTile.count(2, index.isEven ? 3 : 4);
-            //     },
-            //     // staggeredTileBuilder: (int index) =>
-            //     //     new StaggeredTile.count(2, index.isEven ? 2 : 1),
-            //     mainAxisSpacing: 4.0,
-            //     crossAxisSpacing: 4.0,
-            //   ),
-            // ),
-            ),
-      ),
+                    )
+                  ]),
+                ),
+              );
+            },
+          )
+          //   StaggeredGridView.countBuilder(
+          //     // controller: _staggredGridController,
+          //     physics:
+          //         NeverScrollableScrollPhysics(), // không scroll theo widget này
+          //     // shrinkWrap: true,
+          //     scrollDirection: Axis.vertical,
+          //     crossAxisCount: 4,
+          //     itemCount: images.length,
+          //     itemBuilder: (BuildContext context, int index) {
+          //       return Container(
+          //         child: Stack(children: [
+          //           Container(
+          //             padding:
+          //                 EdgeInsets.only(top: 5, left: 5, right: 5, bottom: 50),
+          //             decoration: BoxDecoration(
+          //                 color: Colors.white,
+          //                 // color: 100 * (index % 9) == 0
+          //                 //     ? Colors.pink[50]
+          //                 //     : Colors.pink[100 * (index % 9)],
+          //                 borderRadius: BorderRadius.circular(8),
+          //                 boxShadow: [
+          //                   BoxShadow(
+          //                       color: Colors.grey.withOpacity(0.5),
+          //                       blurRadius: 1,
+          //                       offset: Offset(2, 2))
+          //                 ]),
+          //             child: SizedBox(
+          //               child: Container(
+          //                 decoration: BoxDecoration(
+          //                   borderRadius: BorderRadius.circular(8),
+          //                   image: DecorationImage(
+          //                       image: AssetImage(images[index]),
+          //                       fit: BoxFit.cover),
+          //                 ),
+          //               ),
+          //             ),
+          //           ),
+          //           Positioned(
+          //             bottom: 8,
+          //             child: Container(
+          //               alignment: Alignment.centerLeft,
+          //               child: Padding(
+          //                 padding: const EdgeInsets.all(8),
+          //                 child: Text(
+          //                   'Hellllo',
+          //                   style: TextStyle(color: Colors.black, fontSize: 16),
+          //                 ),
+          //               ),
+          //             ),
+          //           )
+          //         ]),
+          //       );
+          //     },
+          //     // ignore: missing_return
+          //     staggeredTileBuilder: (index) {
+          //       return new StaggeredTile.count(2, index.isEven ? 3 : 4);
+          //     },
+          //     // staggeredTileBuilder: (int index) =>
+          //     //     new StaggeredTile.count(2, index.isEven ? 2 : 1),
+          //     mainAxisSpacing: 4.0,
+          //     crossAxisSpacing: 4.0,
+          //   ),
+          // ),
+          ),
     ));
   }
 }
